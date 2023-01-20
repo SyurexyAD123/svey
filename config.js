68b129f753582d74737fbae2f705e21705b3a5f0
SyurexyAD123 <123092345+SyurexyAD123@users.noreply.github.com>
2023-01-20 12:59:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,37 +6,37 @@ untuk lainya saran jangan
 di ubah bre nanti eror]]*/
 
 /*global owner*/
-global.owner = ['6281395861695']  
-global.mods = ['6281395861695'] 
-global.prems = ['6281395861695']
+global.owner = ['6281271010619']  
+global.mods = ['6281271010619'] 
+global.prems = ['6281271010619']
 /*Nomor Owner & payment*/
-global.nameowner = 'Tio'
-global.numberowner = '6281395861695' 
-global.mail = 'tioclkp02@gmail.com' //enter your email 🗿
-global.dana = '6281395861695'
-global.pulsa = '6281395861695'
-global.gopay = '6281395861695'
+global.nameowner = 'Suranta AD'
+global.numberowner = '6281271010619' 
+global.mail = '-' //enter your email 🗿
+global.dana = '-'
+global.pulsa = '-'
+global.gopay = '-'
 /*Nama Bot & Grouplink*/
-global.namebot = 'RTXZY-MD'
-global.gc = 'https://chat.whatsapp.com/D6BZVRSymWaFU28t2lxaO0'
-global.web = 'https://github.com/BOTCAHX'
-global.instagram = 'https://instagram.com/prm2.0'
+global.namebot = 'Syurexy-BOT'
+global.gc = '-'
+global.web = '-'
+global.instagram = '-'
 /*global wm*/
 global.lolkey = 'rey2k22'
 global.zenzkey = 'BagasPrdn'
-global.wm = '© R-TXZY-MD'
+global.wm = '© Syurexy-MD'
 global.watermark = wm
-global.wm2 = '⫹⫺ R-TXZY-MD'
-global.wm3 = '© R-TXZY-MD'
-global.wm4 = '© BOT RTXZY'
+global.wm2 = '⫹⫺ SYUREXY-MD'
+global.wm3 = '© SYUREXY-MD'
+global.wm4 = '© SYUREXYBOT'
 global.fla = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&script=water-logo&fontsize=90&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextColor=%23000&shadowGlowColor=%23000&backgroundColor=%23000&text='
 global.wait = '_*Tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
 global.benar = 'Benar ✅\n'
 global.salah = 'Salah ❌\n'
 global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made with'
-global.author = 'Bot WhatsApp\nR-TXZY-MD'
+global.packname = 'punya'
+global.author = 'SYUREXY-BOT'
 /*Rest api setting*/
 global.APIs = { // API Prefix
   // name: 'https://website'
